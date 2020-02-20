a4de22b6068b305dce8b4e79b1c79bf5ec446904
robbymo81 <robert.l.mosher@gmail.com>
2020-02-20 13:07:05 -0800

    adding folder14

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -1,13 +1,13 @@
-'use strict';
-/////////////////////////////////////
-/* Problem 1 (this is your demo that we'll solve in class)
-Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
+// 'use strict';
+// /////////////////////////////////////
+// /* Problem 1 (this is your demo that we'll solve in class)
+// Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
-"The sum of 4 and 7 is 11."
+// "The sum of 4 and 7 is 11."
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
+// Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-// Write your code here
+// // Write your code here
  
 function sum (num1, num2) { //eslint-disable-line
   let result = num1 + num2;
@@ -48,29 +48,30 @@ function multiply(num1, num2) { //eslint-disable-line
 testMultiply();
 //multiply(5,9);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+// // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
-/////////////////////////////////////
-/* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  
+// /////////////////////////////////////
+// /* Problem 3
+// Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  
 //and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 
-Third element: "4 and 7 and 5 sum to 16."
-Fourth element: "The product of 4 and 7 and 5 is 140."
+// Third element: "4 and 7 and 5 sum to 16."
+// Fourth element: "The product of 4 and 7 and 5 is 140."
 
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+// IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
+// Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
 //remember that the  func line is sumAndMultiply
+
 //Iris Leal helped me understand how to pull information out of an Array
 function sumAndMultiply(num1, num2, num3) { //eslint-disable-line
 //I need to add numbers with using an operation
  let sumNum1AndNum2 = sum(num1,num2)[0];
  let sumResult = sum(sumNum1AndNum2,num3)[0];
  
- let thirdElement = (num1+ ' and ' + num2+ ' and ' + num3 + ' sum to ' + sumResult +'.');
+ let thirdElement = (num1 + ' and ' + num2 + ' and ' + num3 + ' sum to ' + sumResult +'.');
  
  console.log(sumResult);
 
@@ -79,14 +80,13 @@ function sumAndMultiply(num1, num2, num3) { //eslint-disable-line
 
  let fourthElement = ('The product of ' + num1 + ' and ' + num2 + ' and ' + num3 + ' is ' + productResult +'.');
  console.log(fourthElement);
- //if (sumAndMultiply(4, 7, 5)[0] === 16 && sumAndMultiply(4, 7, 5)[1] === 140 && sumAndMultiply(4, 7, 5)[2] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[3] === 'The product of 4 and 7 and 5 is 140.')
  let myArray = [sumResult, productResult, thirdElement, fourthElement];
   return myArray;
 
 }
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
-//remember that this line should read testSumAndMultiply()
+// // Here is the test for sumAndMultiply(); uncomment it to run it
+// //remember that this line should read testSumAndMultiply()
 testSumAndMultiply();
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -104,13 +104,17 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
-
+function sumArray(sumArr) { //eslint-disable-line;;
+  let add2 = sum(testArray[0],testArray[1]);
+  let add3 = sum(add2,testArray[2]);
+  
+  console.log('adding arrays ' + add2, add3);
 }
+//let returnArray = [9, '2,3,4 was passed in as an array of numbers, and ' + ]}
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -125,9 +129,9 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
+//function multiplyArray(multArr) { //eslint-disable-line
 
-}
+//}
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
@@ -140,24 +144,24 @@ function multiplyArray(multArr) { //eslint-disable-line
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+// Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
 
-"The numbers 1,2,3,4,5 have a product of 120."
+// "The numbers 1,2,3,4,5 have a product of 120."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+// IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
-This function should be dynamic, accepting an array of any length.
+// This function should be dynamic, accepting an array of any length.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
+// Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+// var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
-function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+// function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
-}
+// }
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyAnyArray(testDynamicArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in
--- class-05/lab/starter-code/tests.js
+++ class-05/lab/starter-code/tests.js
@@ -5,6 +5,7 @@
 function testSum() { //eslint-disable-line
   if (sum(4,7)[0] === 11 && sum(4,7)[1] === 'The sum of 4 and 7 is 11.') {
     console.log('%c TEST FOR sum() PASSES', 'color: green');
+    console.log(sum)
   } else {
     console.log('%c TEST FOR sum() FAILS', 'color: red');
   }
